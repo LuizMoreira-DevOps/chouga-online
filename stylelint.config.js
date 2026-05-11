bff64309b9ad964d237c861b67ee01f8736fee49
Luiz Moreira <luiz.moreira1@pucpr.edu.br>
2026-05-11 13:17:23 -0300

    Create ci-cd

---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ["stylelint-config-standard"],
+
+  rules: {
+    "no-empty-source": null,
+
+    "font-family-name-quotes": null,
+
+    "color-function-notation": null,
+
+    "alpha-value-notation": null,
+
+    "color-function-alias-notation": null,
+
+    "declaration-empty-line-before": null,
+
+    "keyframes-name-pattern": null,
+
+    "selector-class-pattern": null,
+
+    "no-descending-specificity": null,
+  },
+};
