318cf3e2c0a1700a6d21fb01e26140c68db3710f
Luiz Moreira <luiz.moreira1@pucpr.edu.br>
2026-05-12 10:27:58 -0300

    fix router basename for github pages

--- Chouga-online/src/main.jsx
+++ Chouga-online/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from "./App.jsx";
 import "./css/index.css";
 import "./css/global.css";
 import "@fontsource/oswald";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/chouga-online">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
