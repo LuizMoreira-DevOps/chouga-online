cfa750d4874496563785b6086d37bd9bc66468c3
Luiz Moreira <luiz.moreira1@pucpr.edu.br>
2026-05-13 11:37:37 -0300

    Update
    Ajuste das rotas.
    Ajuste dos ícones lock e frase "EM BREVE", no menu Wheel.

---
+++ Chouga-online/src/pages/EmBreve.jsx
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+
+function EmBreve() {
+  return (
+    <Layout>
+      <section className="coming-soon">
+        <div className="coming-soon-content">
+          <span className="coming-tag">CHOUGA ONLINE</span>
+
+          <h1>EM BREVE</h1>
+
+          <p>
+            Estamos preparando algo pesado.
+            <br />
+            Novos produtos chegando em breve.
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default EmBreve;
