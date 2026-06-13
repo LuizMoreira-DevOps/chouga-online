e23ed1c144c3e4b88f178aee1663fc0b0fe22d61
Luiz Moreira <264183736+LuizMoreira-DevOps@users.noreply.github.com>
2026-06-13 08:06:07 -0300

    chore: ajusta configuração local do Strapi

--- chouga-cms/config/server.js
+++ chouga-cms/config/server.js
@@ -1,10 +1,22 @@
+/**
+ * @param {{
+ *   env: {
+ *     (key: string, defaultValue?: string): string;
+ *     int: (key: string, defaultValue?: number) => number;
+ *     array: (key: string) => string[];
+ *     bool: (key: string, defaultValue?: boolean) => boolean;
+ *   };
+ * }} config
+ */
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
