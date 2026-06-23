4bc0a9dcbe48a71202d562f81482a1310740b1ab
Luiz Moreira <264183736+LuizMoreira-DevOps@users.noreply.github.com>
2026-06-23 13:24:59 -0300

    fix: remove chouga-online basename for custom domain

--- Chouga-online/src/main.jsx
+++ Chouga-online/src/main.jsx
@@ -11,12 +11,12 @@ const redirectPath = sessionStorage.getItem("redirectPath");
 
 if (redirectPath) {
   sessionStorage.removeItem("redirectPath");
-  window.history.replaceState(null, "", `/chouga-online${redirectPath}`);
+  window.history.replaceState(null, "", redirectPath);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/chouga-online">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
