cdfd31e408c4a02e45124ad17dd98159cd26225f
Luiz Moreira <luiz.moreira1@pucpr.edu.br>
2026-09-17 20:00:05 -0300

    feat: aprimorar experiência responsiva da página de eventos

--- Chouga-online/src/components/events/EventsCarousel.jsx
+++ Chouga-online/src/components/events/EventsCarousel.jsx
@@ -49,7 +49,7 @@ function EventsCarousel({
               >
                 {event.image ? (
                   <img
-                    src={event.image.detailUrl ?? event.image.cardUrl}
+                    src={event.image.detailUrl}
                     alt={event.image.alt}
                     loading="lazy"
                     width="720"
